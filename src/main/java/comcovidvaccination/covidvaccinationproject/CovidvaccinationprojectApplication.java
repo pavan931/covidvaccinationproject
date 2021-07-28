65bf1c3c555504bc856a415b929ee50a31cc395c
pavan <pavand305@gmail.com>
2021-07-28 03:37:57 -0500

    1.0

---
+++ src/main/java/comcovidvaccination/covidvaccinationproject/CovidvaccinationprojectApplication.java
@@ -0,0 +1,13 @@
+package comcovidvaccination.covidvaccinationproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidvaccinationprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidvaccinationprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/comcovidvaccination/covidvaccinationproject/CovidvaccinationprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package comcovidvaccination.covidvaccinationproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidvaccinationprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
